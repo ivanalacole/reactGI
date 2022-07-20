600211c069a973246d6127a67ce2f39bc6bb5996
Ivana Burton Thompson <IvanaBT@R2Hs-MacBook-Pro.local>
2022-07-20 16:21:54 -0400

    pushing final code

---
+++ reactguided/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+// import BasicInfo from './Components/Medium.jsx';
+import BasicInfoList from './Components/Hard.jsx';
+
+const App = () => {
+  return (
+    <div className="App">
+
+      {/* <BasicInfo /> */}
+
+      <BasicInfoList />
+
+    </div>
+  );
+}
+
+export default App;
